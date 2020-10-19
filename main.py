2d4a7de960f840e0bdb081cca58a5751a4271544
Seyit Süleyman Tekin <Stekin19@ku.edu.tr>
2020-10-19 17:36:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+x = input("Type anything... ")
+x = int(x)
+y = input("Type anything... ")
+y = int(y)
+print(x**y)
+
+import math
+x = input("Type anything... ")
+x = int(x)
+print(math.log(x, 2))
